27fb44ca69c8404ea1079964e5764af58083f129
Sergey <Sergey@DESKTOP-57H400F>
2024-08-25 00:39:59 +0500

    Начата работа с программой

---
+++ Microinvest1cData/Sqlitecontroller.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microinvest1cData
+{
+   public  class Sqlitecontroller
+    {
+        private String ConnectionString = "";
+        private SQLiteConnection sqliteConnection;
+        //private bool flag;
+        public Sqlitecontroller()
+        {
+            if (!File.Exists(@"microinvest.db")) // если базы данных нету, то...
+            {
+                SQLiteConnection.CreateFile(@"microinvest.db");
+                sqliteConnection = new SQLiteConnection("Data Source=microinvest.db;Version=3;");
+                //flag = true;
+            }
+            else
+            {
+                sqliteConnection = new SQLiteConnection("Data Source=microinvest.db;Version=3;");
+            }
+        }
+        private void Open()
+        {
+
+            sqliteConnection.Open();
+        }
+
+        private void Close()
+        {
+            sqliteConnection.Close();
+        }
+
+        private void SqlNotQuery(SQLiteCommand command)
+        {
+            command.Connection = sqliteConnection;
+            command.ExecuteNonQuery();
+        }
+        private SQLiteDataReader DataReader(SQLiteCommand command)
+        {
+            command.Connection = sqliteConnection;
+            return command.ExecuteReader();
+        }
+
+        private void CreateTable()
+        {
+            Open();
+            SqlNotQuery(new SQLiteCommand { CommandText = "" });
+            Close();
+        }
+
+    }
+}
